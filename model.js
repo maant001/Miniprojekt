a35df946f2b8b18e5c0d0894b0655f3fa8f2b5bc
maant001 <71355873+maant001@users.noreply.github.com>
2022-11-14 17:13:02 +0100

    extracted model into separate file, still works

--- model.js
+++ model.js
@@ -1,1 +1,41 @@
 /* TODO Aufgabe 2*/
+function elt(type, attrs, ...children) {
+    let node = document.createElement(type)
+    for (a in attrs) {
+        node.setAttribute(a, attrs[a])
+    }
+    for (let child of children) {
+        if (typeof child != "string") {
+            node.appendChild(child)
+        } else {
+            node.appendChild(document.createTextNode(child))
+        }
+    }
+    return node
+}
+
+function showBoard() {
+    const board = elt("div", {class: "board"})
+
+    for (let row = 0; row < 6; row++) {
+        const row = elt("div", {class: "row"})
+        for (let col = 0; col < 7; col++) {
+            const field = elt("div", {class: "field"})
+
+            if (Math.random() < 0.2) {
+                field.classList.add("piece")
+
+                if (Math.random() < 0.5) {
+                    field.classList.add("blue")
+                } else {
+                    field.classList.add("red")
+                }
+            }
+            row.appendChild(field)
+        }
+        board.appendChild(row)
+    }
+    document.body.appendChild(board)
+}
+
+showBoard()
