55698d20a29e80c1c032e8b3598bc81be12d459c
maant001 <71355873+maant001@users.noreply.github.com>
2022-11-25 12:44:08 +0100

    current player turn show

--- model.js
+++ model.js
@@ -1,4 +1,14 @@
+// variables
+let rows = 6
+let columns = 7
+let state = {
 
+}
+let currentPlayer = "red"
+let board
+
+
+//functions
 function elt(type, attrs, ...children) {
     let node = document.createElement(type)
     for (a in attrs) {
@@ -14,6 +24,7 @@ function elt(type, attrs, ...children) {
     return node
 }
 
+// TODO
 function showBoard() {
     const board = elt("div", {class: "board"})
 
@@ -22,15 +33,6 @@ function showBoard() {
         for (let col = 0; col < 7; col++) {
             const field = elt("div", {class: "field"})
 
-            if (Math.random() < 0.2) {
-                field.classList.add("piece")
-
-                if (Math.random() < 0.5) {
-                    field.classList.add("blue")
-                } else {
-                    field.classList.add("red")
-                }
-            }
             row.appendChild(field)
         }
         board.appendChild(row)
@@ -39,36 +41,36 @@ function showBoard() {
 }
 
 /* TODO 2 Timer*/
-function timerRandomizer() {
-
-}
+// function timerRandomizer() {
+//
+// }
 
 /* TODO 2 Create EmptyBoard, this gives state of board!*/
 function createEmptyBoard() {
-    let state = Array(6).fill('').map(el => Array(7).fill(''))
-    /*document.body.appendChild(state)*/
-    randomizeField(state)
-    console.log(state)
-}
+     let state = Array(6).fill('').map(el => Array(7).fill(''))
+     /*document.body.appendChild(state)*/
+     randomizeField(state)
+     console.log(state)
+ }
 
 /* TODO 2*/
-function randomizeField(currentBoard) {
-    for (let row in currentBoard) {
-        for (let field in row) {
-            if (Math.random() < 0.5) {
-                if (Math.random() < 0.5) {
-                    currentBoard[row][field] = "b"
-                } else {
-                    currentBoard[row][field] = "r"
-                }
-            }
-        }
-    }
-}
+ function randomizeField(currentBoard) {
+     for (let row in currentBoard) {
+         for (let field in row) {
+             if (Math.random() < 0.5) {
+                 if (Math.random() < 0.5) {
+                     currentBoard[row][field] = "b"
+                 } else {
+                     currentBoard[row][field] = "r"
+                 }
+             }
+         }
+     }
+ }
 
 
-/* TODO 2 testing purposes here*/
+/* TODO 2 calling the functions, testing purposes here*/
 createEmptyBoard()
 showBoard()
-document.getElementsByClassName("board")[0].appendChild(elt("div", {id: "container"}, "gaki"))
+document.getElementsByClassName("board")[0].appendChild(elt("div", {id: "container"}, currentPlayer + " now playing"))
 console.log("test")
